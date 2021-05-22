7f1f82d3ab2037fd329a28b842d185aa1f41d355
Ingo Peters <ingo.peters2@gmx.de>
2021-05-22 15:58:22 +0200

    Lecture 23: Placing buildings on our map.

--- City Builder/Assets/Scripts/InputManager.cs
+++ City Builder/Assets/Scripts/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,8 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] LayerMask mouseInputMask;
-
+    [SerializeField] GameObject buildingPrefab = null;
+    [SerializeField] int cellSize = 3;
 
     #region Unity Callbacks
 
@@ -29,9 +31,24 @@ public class InputManager : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
                 Vector3 position = hit.point - transform.position;
-                Debug.Log(position);
+                Debug.Log(GetGridPosition(position));
+                CreateBuilding(GetGridPosition(position));
             }
         }
+
+    }
+
+    private void CreateBuilding(Vector3 gridPosition)
+    {
+        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+    }
+
+    private Vector3 GetGridPosition(Vector3 inputPosition)
+    {
+        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+
+        return new Vector3(x * cellSize, 0, z * cellSize);
     }
 
     #endregion
