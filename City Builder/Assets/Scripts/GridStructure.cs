afb9fb2d9e980df8d363ef37586eb58de8079d0d
Ingo Peters <ingo.peters2@gmx.de>
2021-05-22 16:10:05 +0200

    Lecture 24: - Refacored code: Created a separate class for GridStructure. - Implemented Unit Test for GridStructure.

---
+++ City Builder/Assets/Scripts/GridStructure.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridStructure
+{
+    private int cellSize;
+
+    public Vector3 GetGridPosition(Vector3 inputPosition)
+    {
+        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+
+        return new Vector3(x * cellSize, 0, z * cellSize);
+    }
+
+}
--- City Builder/Assets/Scripts/InputManager.cs
+++ City Builder/Assets/Scripts/InputManager.cs
@@ -7,8 +7,7 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] LayerMask mouseInputMask;
-    [SerializeField] GameObject buildingPrefab = null;
-    [SerializeField] int cellSize = 3;
+    //[SerializeField] GameObject buildingPrefab = null;
 
     #region Unity Callbacks
 
@@ -31,25 +30,16 @@ public class InputManager : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
                 Vector3 position = hit.point - transform.position;
-                Debug.Log(GetGridPosition(position));
-                CreateBuilding(GetGridPosition(position));
             }
         }
 
     }
 
-    private void CreateBuilding(Vector3 gridPosition)
-    {
-        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
-    }
+    //private void CreateBuilding(Vector3 gridPosition)
+    //{
+    //    Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+    //}
 
-    private Vector3 GetGridPosition(Vector3 inputPosition)
-    {
-        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
-        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
-
-        return new Vector3(x * cellSize, 0, z * cellSize);
-    }
 
     #endregion
 }
